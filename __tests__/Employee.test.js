b709b1bce0633670e6836b5325bfe82607475f8c
Mohit Arora <mohit.arora@sydney.edu.au>
2021-05-18 01:47:22 +1000

    added employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,22 +1,51 @@
 const Employee = require('../lib/Employee');
 
-// can instantiate Employee instance
 describe("Initialization", () => {
-test('Can instantiate Employee instance', () => {
-    const employee = new Employee ();
-    expe
-});
+    // can instantiate Employee instance
+    it('Can instantiate Employee instance', () => {
+        const employee = new Employee ();
+        expect(typeof employee).toBe("object");
+    });
 
-// can set name via constructor arguments
-test('Can set name via constructor arguments', () => {
-    const employee = new Employee ('David')
-});
+    // can set name via constructor arguments
+    it('Can set name via constructor arguments', () => {
+        const employee = new Employee ("David");
+        expect(employee.name).toBe("David");
+    });
 
-// can set name via constructor arguments
-// can set id via constructor arguments
-// can set email via constructor arguments
-// can get name via getName()
-// can get id via getId()
-// can get email via getEmail()
-// getRole() should return "Employee"
+    // can set id via constructor arguments
+    it('Can set id via constructor arguments', () => {
+        const employee = new Employee ("David", 1);
+        expect(employee.id).toBe(1);
+    });
+
+    // can set email via constructor arguments
+    it('Can set email via constructor arguments', () => {
+        const employee = new Employee ("David", 1, "d.impey@bcs.com.au");
+        expect(employee.email).toBe("d.impey@bcs.com.au");
+    });
+
+    // can get name via getName()
+    it('Can get name via getName()', () => {
+        const employee = new Employee ("David", 1, "d.impey@bcs.com.au");
+        expect(employee.getName()).toBe("David");
+    });
+    
+    // can get id via getId()
+    it('Can get id via getId()', () => {
+        const employee = new Employee ("David", 1, "d.impey@bcs.com.au");
+        expect(employee.getID()).toBe(1);
+    });
+    
+    // can get email via getEmail()
+    it('Can get email via getEmail()', () => {
+        const employee = new Employee ("David", 1, "d.impey@bcs.com.au");
+        expect(employee.getEmail()).toBe("d.impey@bcs.com.au");
+    });
+    
+    // getRole() should return "Employee"
+    it('getRole() should return "Employee"', () => {
+        const employee = new Employee ("David", 1, "d.impey@bcs.com.au");
+        expect(employee.getRole()).toBe("Employee");
+    });
 });
