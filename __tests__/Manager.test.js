bf22f6f691943aa10cb5b3eb2f98a116fff886c5
Mohit Arora <mohit.arora@sydney.edu.au>
2021-05-18 01:56:24 +1000

    added test to manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,26 @@
-// can set office number via constructor arguments
-// getRole() should return "Manager"
-// can get office number via getOfficeNumber()
+const Employee = require('../lib/Employee.js');
+const Manager = require('../lib/Manager.js');
+
+describe("Initialization", () => {
+    // can set office number via constructor arguments
+    it('Can set office number via constructor arguments', () => {
+        const manager = new Manager ("David", 1, "d.impey@bcs.com.au", 98765432);
+        expect(employee.officeNumber).toBe(98765432);
+    });
+
+    // can get office number via getOfficeNumber()
+    it('Can get office number via getOfficeNumber()', () => {
+        const manager = new Manager ("David", 1, "d.impey@bcs.com.au", 98765432);
+        expect(employee.getOfficeNumber()).toBe(98765432);
+    });
+
+    // getRole() should return "Manager"
+    it('getRole() should return "Manager"', () => {
+        const manager = new Manager ("David", 1, "d.impey@bcs.com.au", 98765432);
+        expect(employee.getRole()).toBe("Manager");
+    });
+});
+
+
+// 
+// 
